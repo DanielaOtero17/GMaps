3bd7b984d053cbb25c7e7adf9d67c098410875ce
Dilan Bustamante <dilanbustmart@gmail.com>
2019-08-10 12:30:17 -0500

    GMAP en proceso

--- Taller GMaps/Taller GMaps/Form1.cs
+++ Taller GMaps/Taller GMaps/Form1.cs
@@ -7,14 +7,39 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using GMap.NET;
+using GMap.NET.MapProviders;
+using GMap.NET.WindowsForms;
+using GMap.NET.WindowsForms.Markers;
+
 
 namespace Taller_GMaps
 {
+
     public partial class Form1 : Form
     {
+
+        GMarkerGoogle market;
+        GMapOverlay marketOverplay;
+
+        double latInicial = 20.968813;
+        double longInicial = 89.625091;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            gMapControl1.DragButton = MouseButtons.Left;
+            gMapControl1.CanDragMap = true;
+            gMapControl1.MapProvider = GMapProviders.GoogleMap;
+            gMapControl1.Position = new PointLatLng(latInicial, longInicial);
+            gMapControl1.MinZoom = 0;
+            gMapControl1.MaxZoom = 24;
+            gMapControl1.Zoom = 9;
+            gMapControl1.AutoScroll = true;
         }
     }
 }
